628af32c5e530fb9be6b1a222d354323bc6869bb
kotaroito <ikotaro@students.cs.ubc.ca>
2022-11-03 17:24:32 -0700

    Added the functionality to display the link

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 class Product {
-    constructor(name, price, image) {
+    constructor(name, price, image, link) {
         this.name = name;
         this.price = price;
         this.image = image;
+        this.link = link;
     }
 
     getName() {
@@ -17,6 +18,10 @@ class Product {
         return this.image;
     }
 
+    getLink() {
+        return this.link;
+    }
+
     setId(id) {
         this.id = id;
     }
@@ -26,8 +31,20 @@ class Product {
     }
 }
 
-const products = [new Product("Apple", 5, "https://m.media-amazon.com/images/I/818VhMc24JL._AC_SL1500_.jpg"), 
-new Product("Orange", 6, "https://m.media-amazon.com/images/I/818VhMc24JL._AC_SL1500_.jpg")];
+// Constant declaration: products and buttons
+const products = 
+[
+    new Product("Apple", 1.15, "https://assets.shop.loblaws.ca/products/20073899001/b2/en/front/20073899001_front_a06_@2.png", "https://www.nofrills.ca/red-delicious-apples/p/20073899001_KG"), 
+    new Product("Orange", 6.99, "https://assets.shop.loblaws.ca/products/20323404001/b2/en/front/20323404001_front_a06_@2.png", "https://www.nofrills.ca/organic-navel-oranges-3lb-bag/p/20323404001_EA"), 
+    new Product("Grape", 3.74, "https://assets.shop.loblaws.ca/products/20254491001/b2/en/front/20254491001_front_a06_@2.png", "https://www.nofrills.ca/red-seedless-grapes/p/20254491001_EA"),
+    new Product("Kiwi", 0.75, "https://assets.shop.loblaws.ca/products/20130242001/b2/en/front/20130242001_front_a06_@2.png", "https://www.nofrills.ca/kiwis/p/20130242001_EA"),
+    new Product("Pineapple", 3.50, "https://assets.shop.loblaws.ca/products/20160992001/b2/en/front/20160992001_front_a06_@2.png", "https://www.nofrills.ca/pineapple/p/20160992001_EA"),
+    new Product("Melon", 4.00, "https://assets.shop.loblaws.ca/products/20167017001/b2/en/front/20167017001_front_a06_@2.png", "https://www.nofrills.ca/cantaloupe/p/20167017001_EA"),
+    new Product("Watermelon", 6.00, "https://assets.shop.loblaws.ca/products/20145379001/b2/en/front/20145379001_front_a06_@2.png", "https://www.nofrills.ca/mini-seedless-watermelon/p/20145379001_EA"),
+    new Product("Lemon", 0.99, "https://assets.shop.loblaws.ca/products/20028593001/b2/en/front/20028593001_front_a06_@2.png", "https://www.nofrills.ca/lemon/p/20028593001_EA"),
+    new Product("Strawberry", 5.99, "https://assets.shop.loblaws.ca/products/20049778001/b2/en/front/20049778001_front_a06_@2.png", "https://www.nofrills.ca/strawberries-1lb/p/20049778001_EA"),
+    new Product("Banana", 1.75, "https://assets.shop.loblaws.ca/products/20175355001/b2/en/front/20175355001_front_a06_@2.png", "https://www.nofrills.ca/bananas-bunch/p/20175355001_KG")
+];
 const buttons = document.querySelectorAll(".num-btn");
 const searchId = document.getElementById("search-id");
 const main = document.getElementById("main");
@@ -104,10 +121,22 @@ document.getElementById("search").addEventListener("click", function(){
     let productPrice = createText();
     productPrice.innerHTML = "Price: $" + String(nextProduct.getPrice());
 
+    let buyDiv = document.createElement("div");
+    buyDiv.classList.add("text-center");
+
+    let buy = document.createElement("a");
+    buy.href = nextProduct.getLink();
+    buy.target = "_blank";
+    buy.innerHTML = "Buy this product";
+    
+    buyDiv.append(buy);
+    
     let description = document.getElementById("description");
     description.innerHTML = "";
     description.append(productName);
     description.append(productPrice);
+    description.append(buyDiv);
+    
 
     searchId.value = "";
 });
